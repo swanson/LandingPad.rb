2a970afbd03bc3384943eb6852abdd47f0bb4eb8
Matt Swanson <swanson@purdue.edu>
2011-02-22 18:20:39 -0500

    just kidding

--- landingpad.rb
+++ landingpad.rb
@@ -67,7 +67,7 @@ class LandingPad < Sinatra::Base
     doc = {
       "name"    => contact,
       "type"    => contact_type,
-      "referrer" => params[:referrer],
+      "referer" => request.referer,
     }
    
     $collection.insert(doc)
